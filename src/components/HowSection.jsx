3c1adefdba8251ec1e9ac04789fc4d7e91e3e17d
Iggynp <odissey0x@gmail.com>
2023-05-08 20:12:21 +0200

    improved display + new images

--- src/components/HowSection.jsx
+++ src/components/HowSection.jsx
@@ -10,7 +10,7 @@ const HowSection = () => {
       </div>
       <div className="flex flex-col py-4">
         <div className="flex flex-col lg:px-6 lg:flex-row justify-between items-center text-center mt-8">
-          <Image src="/images/corte-jamon.jpg" width="550" height="550" />
+          <Image src="/images/ordering.jpg" width="550" height="550" />
           <div className="flex flex-col px-10">
             <p className="font-bold text-lg mt-4">
               1. Elige el plan que más te guste
@@ -19,15 +19,15 @@ const HowSection = () => {
             <PlanesBtn />
           </div>
         </div>
-        <div className="flex flex-col lg:px-6 lg:flex-row justify-between items-center text-center mt-8">
+        <div className="flex flex-col-reverse lg:px-6 lg:flex-row justify-between items-center text-center mt-8">
           <div className="flex flex-col px-10">
             <p className="font-bold text-lg mt-4">2. Preparamos tu pedido</p>
             <p>Y te lo enviamos el lunes más cercano.</p>
           </div>
-          <Image src="/images/jamon-cerca.jpg" width="550" height="550" />
+          <Image src="/images/delivery.jpg" width="550" height="550" />
         </div>
         <div className="flex flex-col lg:px-6 lg:flex-row justify-between items-center text-center mt-8">
-          <Image src="/images/manos-movil.jpg" width="550" height="550" />
+          <Image src="/images/jamon-focus.jpg" width="550" height="550" />
           <div className="flex flex-col px-10">
             <p className="font-bold text-lg mt-4">
               3. Recíbelo en tu casa y disfruta
@@ -35,14 +35,14 @@ const HowSection = () => {
             <p>Llegará entre 24-48 horas desde que se envía.</p>
           </div>
         </div>
-        <div className="flex flex-col lg:px-6 lg:flex-row justify-between items-center text-center mt-8">
+        <div className="flex flex-col-reverse lg:px-6 lg:flex-row justify-between items-center text-center mt-8">
           <div className="flex flex-col px-10">
             <p className="font-bold text-lg mt-4">
               4. Espera con ganas el mes que viene
             </p>
             <p>Para recibir más jamón sin preocuparte por nada!</p>
           </div>
-          <Image src="/images/manos-movil.jpg" width="550" height="550" />
+          <Image src="/images/waiting.jpg" width="550" height="550" />
         </div>
       </div>
       <div className="flex flex-col py-6 justify-center items-center text-center mt-20">
