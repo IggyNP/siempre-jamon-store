a50d7001979031d54e40138de0b5849a7e981196
Iggynp <odissey0x@gmail.com>
2023-05-11 21:25:17 +0200

    changed PlanesBtn to match the general theme

--- src/components/PlanesBtn.jsx
+++ src/components/PlanesBtn.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const PlanesBtn = () => {
   return (
     <Link href="/planes">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-3 py-2 border border-blue-500 hover:border-transparent rounded uppercase mt-6 md:text-md lg:text-lg">
+      <button className="bg-[#DC2626] hover:bg-[#B91C1C] duration-300 text-white font-bold px-3 py-2 border border-blue-500 hover:border-transparent rounded uppercase mt-6 md:text-md lg:text-lg">
         Descubre los planes
       </button>
     </Link>
