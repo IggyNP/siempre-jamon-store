bdf86088c4dbcf7ee9102c4d96bc1e355f8003ac
Iggynp <odissey0x@gmail.com>
2023-05-15 14:16:23 +0200

    PlanTypeBtn is now smooth

--- src/components/PlanTypeBtn.jsx
+++ src/components/PlanTypeBtn.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const PlanTypeBtn = () => {
   return (
     <div>
-      <div className="flex items-center justify-center mt-6 lg:mt-12">
-        <div className="flex bg-white rounded-full shadow-lg">
+      <div className="flex justify-center mt-6 lg:mt-12">
+        <div className="relative inline-flex rounded-full bg-white shadow-lg">
           <input
             id="jamón"
             type="radio"
             name="switch"
-            className="opacity-0 absolute peer/jamón"
+            className="hidden peer/jamón"
             checked
           />
           <label
             for="jamón"
-            className="leading-none hover:cursor-pointer text-gray-600 rounded-full py-4 px-6 peer-checked/jamón:cursor-default peer-checked/jamón:bg-[#DC2626] peer-checked/jamón:text-white peer-checked/jamón:rounded-full peer-checked/jamón:py-4 peer-checked/jamón:px-6"
+            className="select-none z-10 leading-none cursor-pointer rounded-full py-4 px-6 peer-checked/jamón:cursor-default peer-checked/jamón:text-white transition-all duration-300 ease-in-out"
           >
             Jamón
           </label>
@@ -22,14 +22,15 @@ const PlanTypeBtn = () => {
             id="paletilla"
             type="radio"
             name="switch"
-            className="opacity-0 absolute peer/paletilla"
+            className="hidden peer/paletilla"
           />
           <label
             for="paletilla"
-            className=" leading-none hover:cursor-pointer text-gray-600 rounded-full py-4 px-6 peer-checked/paletilla:cursor-default peer-checked/paletilla:bg-[#DC2626] peer-checked/paletilla:text-white peer-checked/paletilla:rounded-full peer-checked/paletilla:py-4 peer-checked/paletilla:px-6"
+            className="select-none z-10 leading-none cursor-pointer rounded-full py-4 px-6 peer-checked/paletilla:cursor-default peer-checked/paletilla:text-white transition-all duration-300 ease-in-out"
           >
             Paletilla
           </label>
+          <div className="absolute inset-y-0 left-0 z-0 w-1/2 rounded-full bg-[#DC2626] transform peer-checked/paletilla:translate-x-full transition-all duration-300 ease-in-out"></div>
         </div>
       </div>
     </div>
