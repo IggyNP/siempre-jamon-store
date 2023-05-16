c2bc4b38ddec4f05081e5098377f278552f00551
Iggynp <odissey0x@gmail.com>
2023-05-16 11:07:09 +0200

    Planes shows different plans according to PlanTypeBtn

---
+++ src/components/PlanJamon.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const PlanJamon = () => {
+  return (
+    <div className="relative mt-12 w-full px-4 md:px-8 lg:w-7/12 xl:w-1/2">
+      <img
+        src="images/bg-circles.png"
+        className="absolute -ml-12 mt-24 w-full"
+        alt="background circle images"
+      />
+      <div className="group relative z-30 mt-3 flex cursor-pointer rounded-lg bg-white shadow">
+        <div className="h-auto w-2.5 rounded-bl-md rounded-tl-md bg-[#DC2626] opacity-0 duration-100 ease-in-out group-hover:opacity-100"></div>
+        <div className="w-full p-8">
+          <div className="items-center justify-between md:flex">
+            <h2 className="text-2xl font-semibold leading-6 text-gray-800">
+              El prudente
+            </h2>
+            <p className="mt-4 text-2xl font-semibold text-gray-800">
+              12€ / mes
+            </p>
+          </div>
+          <p className="mt-4 text-gray-600 md:w-80">
+            1 paquete de 150g. de jamón ibérico todos los meses.
+          </p>
+        </div>
+      </div>
+      <div className="group relative z-30 mt-3 flex cursor-pointer rounded-lg bg-white shadow">
+        <div className="h-auto w-2.5 rounded-bl-md rounded-tl-md bg-[#DC2626] opacity-0 duration-100 ease-in-out group-hover:opacity-100"></div>
+        <div className="w-full p-8">
+          <div className="items-center justify-between md:flex">
+            <h2 className="text-2xl font-semibold leading-6 text-gray-800">
+              El moderado
+            </h2>
+            <p className="mt-4 text-2xl font-semibold text-gray-800">
+              22€ / mes
+            </p>
+          </div>
+          <p className="mt-4 text-gray-600 md:w-80">
+            2 paquetes de 150g. de jamón ibérico todos los meses.
+          </p>
+        </div>
+      </div>
+      <div className="group relative z-30 mt-3 flex cursor-pointer rounded-lg bg-white shadow">
+        <div className="h-auto w-2.5 rounded-bl-md rounded-tl-md bg-[#DC2626] opacity-0 duration-100 ease-in-out group-hover:opacity-100"></div>
+        <div className="w-full p-8">
+          <div className="items-center justify-between md:flex">
+            <h2 className="text-2xl font-semibold leading-6 text-gray-800">
+              El campeón
+            </h2>
+            <p className="mt-4 text-2xl font-semibold text-gray-800">
+              40€ / mes
+            </p>
+          </div>
+          <p className="mt-4 text-gray-600 md:w-80">
+            4 paquetes de 150g. de jamón ibérico todos los meses.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlanJamon;
--- src/components/PlanTypeBtn.jsx
+++ src/components/PlanTypeBtn.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 
-const PlanTypeBtn = () => {
+const PlanTypeBtn = ({ setJamonType }) => {
   return (
-    <div>
-      <div className="mt-6 flex justify-center lg:mt-12">
-        <div className="relative inline-flex rounded-full bg-white shadow-lg">
-          <input
-            id="jamón"
-            type="radio"
-            name="switch"
-            className="peer/jamón hidden"
-            defaultChecked
-          />
-          <label
-            htmlFor="jamón"
-            className="z-10 cursor-pointer select-none rounded-full px-6 py-4 leading-none transition-all duration-300 ease-in-out peer-checked/jamón:cursor-default peer-checked/jamón:text-white"
-          >
-            Jamón
-          </label>
-          <input
-            id="paletilla"
-            type="radio"
-            name="switch"
-            className="peer/paletilla hidden"
-          />
-          <label
-            htmlFor="paletilla"
-            className="z-10 cursor-pointer select-none rounded-full px-6 py-4 leading-none transition-all duration-300 ease-in-out peer-checked/paletilla:cursor-default peer-checked/paletilla:text-white"
-          >
-            Paletilla
-          </label>
-          <div className="absolute inset-y-0 left-0 z-0 w-1/2 transform rounded-full bg-[#DC2626] transition-all duration-300 ease-in-out peer-checked/paletilla:translate-x-full"></div>
-        </div>
+    <div className="mt-6 flex justify-center lg:mt-12">
+      <div className="relative inline-flex rounded-full bg-white shadow-lg">
+        <input
+          id="jamón"
+          type="radio"
+          name="switch"
+          className="peer/jamón hidden"
+          defaultChecked
+          onChange={() => setJamonType(true)}
+        />
+        <label
+          htmlFor="jamón"
+          className="z-10 cursor-pointer select-none rounded-full px-6 py-4 leading-none transition-all duration-300 ease-in-out peer-checked/jamón:cursor-default peer-checked/jamón:text-white"
+        >
+          Jamón
+        </label>
+        <input
+          id="paletilla"
+          type="radio"
+          name="switch"
+          className="peer/paletilla hidden"
+          onChange={() => setJamonType(false)}
+        />
+        <label
+          htmlFor="paletilla"
+          className="z-10 cursor-pointer select-none rounded-full px-6 py-4 leading-none transition-all duration-300 ease-in-out peer-checked/paletilla:cursor-default peer-checked/paletilla:text-white"
+        >
+          Paletilla
+        </label>
+        <div className="absolute inset-y-0 left-0 z-0 w-1/2 transform rounded-full bg-[#DC2626] transition-all duration-300 ease-in-out peer-checked/paletilla:translate-x-full"></div>
       </div>
     </div>
   );
